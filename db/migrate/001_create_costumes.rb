c9d999a15089a63016dfe7f86c39f5f356e47f12
Nicholas Keller <nic.keller328@gmail.com>
2020-04-30 03:48:18 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,6 +11,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]# Create a class and inherit 
       t.string :image_url
       t.timestamp :created_at
       t.timestamp :updated_at
+    end
   end
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and
